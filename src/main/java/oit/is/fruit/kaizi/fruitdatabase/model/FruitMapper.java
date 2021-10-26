0c4fdc2897ff2581a444e0f68a3d256f73e36e9c
e1b19041 <e1b19041@oit.ac.jp>
2021-10-26 16:00:58 +0900

    すべての果物レコードを表示する

---
+++ src/main/java/oit/is/fruit/kaizi/fruitdatabase/model/FruitMapper.java
@@ -0,0 +1,19 @@
+package oit.is.fruit.kaizi.fruitdatabase.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface FruitMapper {
+
+  @Select("SELECT ID, NAME,PRICE FROM FRUIT")
+  ArrayList<Fruit> selectAllFruit();
+
+  @Select("select ID, NAME,PRICE from FRUIT WHERE ID = #{id}")
+  Fruit selectById(int id);
+
+}
