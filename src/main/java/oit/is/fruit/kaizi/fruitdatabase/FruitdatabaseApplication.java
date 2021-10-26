9ccc5200e931d83b24edea364cd1644d6ffb2951
e1b19057 <e1b19057@oit.ac.jp>
2021-10-26 14:11:35 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/fruit/kaizi/fruitdatabase/FruitdatabaseApplication.java
@@ -0,0 +1,13 @@
+package oit.is.fruit.kaizi.fruitdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitdatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/fruit/kaizi/fruitdatabase/FruitdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.fruit.kaizi.fruitdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
